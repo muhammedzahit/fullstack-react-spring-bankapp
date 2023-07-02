7e60e6a960203d9627becb89c5cd12a78d61c443
Muhammet Zahit Aydın <muhammetzahitaydin@gmail.com>
2023-07-02 20:14:43 +0300

    install

---
+++ src/main/java/com/example/fullstackspringreactbankingapp/FullstackSpringReactBankingappApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackspringreactbankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackSpringReactBankingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackSpringReactBankingappApplication.class, args);
+	}
+
+}
